632924346e55dfcc65c3ca2c90d3814f10eb24cd
Arc <atandarokib@gmail.com>
2023-07-06 20:57:21 +0100

    up

--- src/Components/Photo.jsx
+++ src/Components/Photo.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import car from "../assets/car.png";
 import Banner2 from "../assets/banner2.png";
 import Banner3 from "../assets/banner3.png";
+import ReactPlayer from "react-player";
 
 const Photo = () => {
   return (
@@ -23,6 +24,21 @@ const Photo = () => {
               data-aos-duration="1500"
             />
           </a>
+          <a href="" className="">
+            <iframe
+              title="vimeo-player"
+              src="https://player.vimeo.com/video/842987937?h=8e825c0061"
+              width="100%"
+              height="360"
+              frameborder="0"
+              allowfullscreen
+            ></iframe>
+          </a>
+          <ReactPlayer url="https://youtu.be/H-gQxhrDYj4" width="100%" />
+          <ReactPlayer
+            url="https://youtube.com/shorts/7cZRkb64zfc?feature=share"
+            width="100%"
+          />
         </article>
       </div>
     </section>
